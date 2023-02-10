43e99ec6a365554bcf608bf21c20e8a6ca11a5a7
Ricardo Maximino <ricardomaximino@hotmail.com>
2023-02-10 16:23:42 +0100

    working to use it as a portifolio template part

--- src/main/java/com/brasajava/webapp/configuration/WebSecurityConfig.java
+++ src/main/java/com/brasajava/webapp/configuration/WebSecurityConfig.java
@@ -2,6 +2,7 @@ package com.brasajava.webapp.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -19,7 +20,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/","/hello", "/index","/public/**").permitAll()
+                .antMatchers(HttpMethod.POST, "/realstate/**").permitAll()
+                .antMatchers("/","/hello", "/index","/public/**", "/realstate/**").permitAll()
                 .antMatchers("/webjars/**", "/js/**", "/css/**", "/image/**", "/assets/**").permitAll()
                 .antMatchers( "/admin/**").hasRole("ADMIN")
                 .antMatchers( "/user/**").hasAnyRole("USER", "ADMIN")
